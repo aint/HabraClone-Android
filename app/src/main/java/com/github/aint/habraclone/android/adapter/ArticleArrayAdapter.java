f793758b91a5215d5bfab937142963080f66f6cc
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2016-08-24 17:26:45 +0300

    Jump to comments on icon click

--- app/src/main/java/com/github/aint/habraclone/android/adapter/ArticleArrayAdapter.java
+++ app/src/main/java/com/github/aint/habraclone/android/adapter/ArticleArrayAdapter.java
@@ -1,6 +1,7 @@
 package com.github.aint.habraclone.android.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
@@ -10,14 +11,16 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.github.aint.habraclone.android.R;
+import com.github.aint.habraclone.android.activity.DisplayCommentsActivity;
 import com.github.aint.habraclone.android.model.Article;
 
 import java.util.Date;
 import java.util.List;
 
+import static com.github.aint.habraclone.android.activity.DisplayArticleActivity.ARTICLE_ID_ATTRIBUTE;
 import static com.github.aint.habraclone.android.activity.MainActivity.DATE_PATTERN;
 
-public class ArticleArrayAdapter extends ArrayAdapter<Article> {
+public class ArticleArrayAdapter extends ArrayAdapter<Article> implements View.OnClickListener {
 
     private final List<Article> articles;
     private final LayoutInflater inflater;
@@ -53,7 +56,14 @@ public class ArticleArrayAdapter extends ArrayAdapter<Article> {
         ((TextView) convertView.findViewById(R.id.article_title_label)).setText(articles.get(position).getTitle());
         ((TextView) convertView.findViewById(R.id.article_hub_label)).setText(articles.get(position).getHubName());
         ((TextView) convertView.findViewById(R.id.article_views_label)).setText(String.valueOf(articles.get(position).getViews()));
-        ((TextView) convertView.findViewById(R.id.article_comments_label)).setText(String.valueOf(articles.get(position).getCommentCount()));
+        setUpCommentsView();
+    }
+
+    private void setUpCommentsView() {
+        TextView commentsTextView = (TextView) convertView.findViewById(R.id.article_comments_label);
+        commentsTextView.setText(String.valueOf(articles.get(position).getCommentCount()));
+        commentsTextView.setTag(position);
+        commentsTextView.setOnClickListener(this);
     }
 
     private void setCreationDate() {
@@ -68,4 +78,9 @@ public class ArticleArrayAdapter extends ArrayAdapter<Article> {
         ratingTextView.setTextColor(rating >= 0 ? (rating == 0 ? Color.LTGRAY : Color.GREEN) : Color.RED);
     }
 
+    @Override
+    public void onClick(View v) {
+        getContext().startActivity(new Intent(getContext(), DisplayCommentsActivity.class)
+                .putExtra(ARTICLE_ID_ATTRIBUTE, articles.get((int) v.getTag()).getId()));
+    }
 }
